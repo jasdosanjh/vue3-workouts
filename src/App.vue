42bf3ff36db0e4d05af0829c14c11f08e27dee5c
Jas Dosanjh <jas@dosanjh.co.uk>
2021-11-19 10:50:15 +0000

    project setup with vite, tailwind, supabase and vue router

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+</style>
