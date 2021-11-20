c78ef13f880c0e0b415be8f3e3d64178f9c266c1
Jas Dosanjh <jas@dosanjh.co.uk>
2021-11-20 17:09:56 +0000

    setup markup for register view!

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-workouts',
+  // TODO: uncomment base when deploying
+  // base: '/vue3-workouts',
   plugins: [vue()],
 });
