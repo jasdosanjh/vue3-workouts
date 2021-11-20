59b4b2f09abddfecb62bd88e893e306397405c66
Jas Dosanjh <jas@dosanjh.co.uk>
2021-11-20 16:49:21 +0000

    scaffolded the navigation component and setup routes

---
+++ src/views/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Register</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/ViewExercise.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>View Exercise</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
