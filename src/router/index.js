a40b593d3a004850a9ab2c17331ebf9e0f36f68c
Jas Dosanjh <jas@dosanjh.co.uk>
2021-11-27 08:21:56 +0000

    implemented route guards to prevent unauthorized access to create view

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { supabase } from '../supabase/init';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
@@ -11,32 +12,65 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Home',
+      auth: false,
+    },
   },
   {
     path: '/login',
     name: 'Login',
     component: Login,
+    meta: {
+      title: 'Login',
+      auth: false,
+    },
   },
   {
     path: '/register',
     name: 'Register',
     component: Register,
+    meta: {
+      title: 'Register',
+      auth: false,
+    },
   },
   {
     path: '/create',
     name: 'Create',
     component: Create,
+    meta: {
+      title: 'Create',
+      auth: true,
+    },
   },
   {
     path: '/workout/:workoutId',
     name: 'Workout',
     component: Workout,
+    meta: {
+      title: 'Workout',
+      auth: false,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Workouts`;
+  next();
+});
+
+router.beforeEach((to, from, next) => {
+  const user = supabase.auth.user();
+  if (to.matched.some((response) => response.meta.auth)) {
+    user ? next() : next({ name: 'Login' });
+  }
+  next();
 });
 
 export default router;
